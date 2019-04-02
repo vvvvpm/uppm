bda5542aa9399bd4614686b3e458a73731e89ff7
David Morasz <morasz@meso.net>
2019-04-02 20:01:39 +0200

    Started to work on GitPackageRepository and GitUtils

--- uppm.Core/GitUtils.cs
+++ uppm.Core/GitUtils.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibGit2Sharp;
 
 namespace uppm.Core.Utils
 {
-    public class GitUtils
+    static public class GitUtils
     {
+        private static void LogVerboseRepoOperationEvent(ILogSource caller, RepositoryOperationContext context)
+        {
+            caller.Log.Debug("    submodule: {SubmoduleName}", context.SubmoduleName);
+            caller.Log.Debug("    recursion depth: {RepoRecDepth}", context.RecursionDepth);
+            caller.Log.Verbose("    parent: {ParentRepoName}", context.ParentRepositoryPath);
+            caller.Log.Verbose("    remote: {RepoRemoteName}", context.RemoteUrl);
+        }
+
+        /// <summary>
+        /// Clone a git repository. Caller must be provided.
+        /// </summary>
+        /// <param name="caller">Required for log and progress origin</param>
+        /// <param name="remote"></param>
+        /// <param name="dst"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// OnCheckoutProgress and OnTransferProgress will be overriden to invoke <see cref="UppmLog.OnAnyProgress"/>.
+        /// OnProgress, RepositoryOperationStarting and RepositoryOperationCompleted will be overriden to log
+        /// in the uppm Serilog system.
+        /// </remarks>
+        public static Repository Clone(ILogSource caller, string remote, string dst, CloneOptions options = null)
+        {
+            options = options ?? new CloneOptions();
+
+            options.OnCheckoutProgress = (path, steps, totalSteps) =>
+            {
+                caller.InvokeAnyProgress(totalSteps, steps, "Checking Out", path);
+            };
+            options.OnTransferProgress = progress =>
+            {
+                caller.InvokeAnyProgress(progress.TotalObjects, progress.ReceivedObjects,"Transferring");
+                return true;
+            };
+            options.OnProgress = output =>
+            {
+                caller.Log.Debug(output);
+                return true;
+            };
+            options.RepositoryOperationStarting = context =>
+            {
+                caller.Log.Information("Repository operation started on {RepoName}", context.RepositoryPath);
+                LogVerboseRepoOperationEvent(caller, context);
+                return true;
+            };
+            options.RepositoryOperationCompleted = context =>
+            {
+                caller.Log.Information("Repository operation completed on {RepoName}", context.RepositoryPath);
+                LogVerboseRepoOperationEvent(caller, context);
+            };
+
+            try
+            {
+                var resultPath = Repository.Clone(remote, dst, options);
+                return new Repository(resultPath);
+            }
+            catch (Exception e)
+            {
+                caller.Log.Fatal(e, "Error during cloning repository {RepoRemoteName}", remote);
+            }
+            return null;
+        }
     }
 }
--- uppm.Core/Repositories/GitPackageRepository.cs
+++ uppm.Core/Repositories/GitPackageRepository.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Flurl.Http;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using md.stdl.String;
 using Serilog;
 using uppm.Core.Scripting;
+using uppm.Core.Utils;
 
 namespace uppm.Core.Repositories
 {
@@ -22,6 +30,10 @@ namespace uppm.Core.Repositories
 
         /// <inheritdoc />
         public Exception RefreshError { get; }
+
+        public Func<string, IFlurlRequest> RequestConfiguration { get; set; }
+
+        public Repository GitRepository { get; private set; }
 
         /// <inheritdoc />
         public bool TryGetPackage(PartialPackageReference reference, out Package package)
@@ -50,13 +62,57 @@ namespace uppm.Core.Repositories
         /// <inheritdoc />
         public bool RepositoryExists()
         {
-            throw new NotImplementedException();
+            if (!RepositoryReferenceValid()) return false;
+            Log.Information("Checking if {RepoUrl} exists?", Url);
+            try
+            {
+                var response = RequestConfiguration == null
+                    ? Url.GetAsync(completionOption: HttpCompletionOption.ResponseHeadersRead)
+                    : RequestConfiguration(Url).GetAsync(completionOption: HttpCompletionOption.ResponseHeadersRead);
+                response.Wait();
+                if (response.Result.StatusCode != HttpStatusCode.OK)
+                {
+                    Log.Error(
+                        "{RepoUrl} doesn't exist. Server responded with {StatusCodeInt} : {$StatusCode}",
+                        Url,
+                        (int)response.Status,
+                        response.Status);
+                    return false;
+                }
+                Log.Verbose("{RepoUrl} does exist", Url);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Error(
+                    e,
+                    "{RepoUrl} doesn't exist. Exception occured.",
+                    Url);
+                return false;
+            }
         }
+
+        public CertificateCheckHandler CertificateCheck { get; set; } = (certificate, valid, host) => true;
+        public CredentialsHandler CredentialsProvider { get; set; } = (url, fromUrl, types) => new DefaultCredentials();
 
         /// <inheritdoc />
         public bool Refresh()
         {
-            throw new NotImplementedException();
+            if (!RepositoryReferenceValid()) return false;
+            var repofolder = Url.MatchGroup(@"^https?\:\/\/(?<reponame>.*?).git[\?\:\$]")["reponame"]
+                .Value
+                .Replace('/', '_');
+
+            //TODO: if repo exist open and checkout (cloning ain't gonna work of course)
+
+            GitRepository = GitUtils.Clone(this, Url,
+                Path.Combine(Uppm.Implementation.TemporaryFolder, repofolder),
+                new CloneOptions
+                {
+                    CertificateCheck = CertificateCheck,
+                    CredentialsProvider = CredentialsProvider,
+                });
+            return GitRepository != null;
         }
 
         /// <inheritdoc />
@@ -67,6 +123,12 @@ namespace uppm.Core.Repositories
 
         /// <inheritdoc />
         public ILogger Log { get; }
+
+        /// <inheritdoc />
+        public event UppmProgressHandler OnProgress;
+
+        /// <inheritdoc />
+        public void InvokeProgress(ProgressEventArgs progress) => OnProgress?.Invoke(this, progress);
 
         public GitPackageRepository()
         {
